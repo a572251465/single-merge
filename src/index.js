a2a280658cabfcbba5911c95d97349521324bf6b
李浩浩 <lihaohao@uino.com>
2021-09-07 12:46:42 +0800

    fix: 修改错误

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ const merge = (...mergeOptions) => {
     mergeOptions.length < 1 ||
     mergeOptions.some((item) => !isArray(item) && !isObject(item))
   ) {
-    throw new Error('The parameter must be an object or an array test')
+    throw new Error('The parameter must be an object or an array')
   }
 
   let mergeTarget = []
